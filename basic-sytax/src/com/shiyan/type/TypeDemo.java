8d1dd265818bec8ebc0a797d1efde777305914b3
Cyan <917244295@qq.com>
2022-11-26 21:08:38 +0800

    type transform

---
+++ basic-sytax/src/com/shiyan/type/TypeDemo.java
@@ -0,0 +1,10 @@
+package com.shiyan.type;
+
+public class TypeDemo {
+    public static void main(String[] args) {
+//      char can be assign to int, but not vise versa
+        char a = 'a';
+        int b = a;
+        System.out.println(b); // 97
+    }
+}
---
+++ hello-app/src/com/shiyan/variable/Variable.java
@@ -0,0 +1,8 @@
+package com.shiyan.variable;
+
+public class Variable {
+    public static void main(String[] args) {
+        double money = 6.0;
+        System.out.println(money);
+    }
+}
