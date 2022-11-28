cdab3007989b356986c6cc3cb29de6eb89301df7
Cyan <917244295@qq.com>
2022-11-28 23:43:18 +0800

    object-oriented

---
+++ object/src/Main.java
@@ -0,0 +1,11 @@
+import Car.Car;
+
+public class Main {
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        c1.name = "奔驰";
+        c1.price = 1000.2;
+        c1.start();
+        c1.run();
+    }
+}
