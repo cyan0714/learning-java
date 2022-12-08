d3426f1c268a78129457991889b6cb089fb4c8ae
Cyan <917244295@qq.com>
2022-12-08 20:42:36 +0800

    login、deposit、draw

--- demo/src/com/shiyan/atm/ATMSystem.java
+++ demo/src/com/shiyan/atm/ATMSystem.java
@@ -22,6 +22,7 @@ public class ATMSystem {
             switch (command) {
                 case 1:
                     // 用户登录
+                    login(accounts, sc);
                     break;
                 case 2:
                     // 账户开户
@@ -32,6 +33,122 @@ public class ATMSystem {
             }
         }
 
+    }
+
+    private static void login(ArrayList<Account> accounts, Scanner sc) {
+        if (accounts.size() == 0) {
+            System.out.println("对不起，当前系统无任何账号，请先开户~");
+            return;
+        }
+
+        while (true) {
+            System.out.println("请输入卡号");
+            String cardId = sc.next();
+
+            Account acc = getAccountByCardId(accounts, cardId);
+
+            if (acc != null) {
+                while (true) {
+                    System.out.println("请输入密码");
+                    String password = sc.next();
+                    if (acc.getPassword().equals(password)) {
+                        System.out.println(acc.getUserName() + "登录成功！");
+                        showUserCommand(sc, acc);
+                    } else {
+                        System.out.println("密码错误");
+                    }
+                }
+            } else {
+                System.out.println("该卡号不存在于系统中~");
+            }
+        }
+
+
+    }
+
+    private static void showUserCommand(Scanner sc, Account acc) {
+        while (true) {
+            System.out.println("================用户操作页===============");
+            System.out.println("1、查询账户");
+            System.out.println("2、存款");
+            System.out.println("3、取款");
+            System.out.println("4、转账");
+            System.out.println("5、修改密码");
+            System.out.println("6、退出");
+            System.out.println("7、注销账户");
+
+            int command = sc.nextInt();
+
+            switch(command) {
+                case 1:
+                    // 展示用户信息
+                    showAccount(acc);
+                    break;
+                case 2:
+                    // 存款
+                    depositMoney(acc, sc);
+                    break;
+                case 3:
+                    // 取款
+                    drawMoney(acc, sc);
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    System.out.println("退出成功，欢迎下次光临~");
+                    return;
+                case 7:
+                    break;
+                default:
+                    System.out.println("您输入的命令不正确！");
+            }
+        }
+    }
+
+    private static void drawMoney(Account acc, Scanner sc) {
+        if (acc.getMoney() < 100) {
+            System.out.println("金额不足，请先存钱~");
+            return;
+        }
+
+        double money = 0;
+        while (true) {
+            System.out.println("请输入取款金额");
+            money = sc.nextDouble();
+
+            if (money > acc.getQuotaMoney()) {
+                System.out.println("取款值大于当次限额" + acc.getQuotaMoney() + "请重新输入~");
+            } else if (acc.getMoney() < money) {
+                System.out.println("存款不足，存点钱吧~");
+            } else {
+                break;
+            }
+
+        }
+
+        acc.setMoney(acc.getMoney() - money);
+        System.out.println("取款成功！");
+        showAccount(acc);
+
+    }
+
+    private static void depositMoney(Account acc, Scanner sc) {
+        System.out.println("请输入存款金额：");
+        double money = sc.nextDouble();
+
+        acc.setMoney(acc.getMoney() + money);
+        System.out.println("存钱成功！账户信息如下：");
+        showAccount(acc);
+    }
+
+    private static void showAccount(Account acc) {
+        System.out.println("当前账户信息如下：");
+        System.out.println("卡号：" + acc.getCardId());
+        System.out.println("户主：" + acc.getUserName());
+        System.out.println("余额：" + acc.getMoney());
+        System.out.println("限额：" + acc.getQuotaMoney());
     }
 
     /**
