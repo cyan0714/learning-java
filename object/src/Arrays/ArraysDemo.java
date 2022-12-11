91410e081a7cecab65c0b3c1435bcf543273248d
Cyan <917244295@qq.com>
2022-12-11 09:52:32 +0800

    compataly lambda

--- object/src/Arrays/ArraysDemo.java
+++ object/src/Arrays/ArraysDemo.java
@@ -1,12 +1,22 @@
 package Arrays;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class ArraysDemo {
     public static void main(String[] args) {
-        int[] arr = {10, 2, 33, 8, 29};
+        Integer[] arr = {10, 2, 33, 8, 29};
         System.out.println(Arrays.toString(arr));
         Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+//        Arrays.sort(arr, new Comparator<Integer>() {
+//            @Override
+//            public int compare(Integer o1, Integer o2) {
+//                return  o2 - o1;
+//            }
+//        });
+        Arrays.sort(arr, (o1, o2) -> o2 - o1);
         System.out.println(Arrays.toString(arr));
         int index = Arrays.binarySearch(arr, 33);
         System.out.println(index);
