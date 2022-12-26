b58378b31f34f0d134ac2b7e3ef412e188257841
cyan0714 <917244295@qq.com>
2022-12-26 11:59:45 +0800

    add chapter2

---
+++ hanshunping/src/com/hsp/chapter2/Homework1.java
@@ -0,0 +1,9 @@
+package com.hsp.chapter2;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        System.out.println("hanshunping\nmale\nbeijing");
+        System.out.println("hanshunping\tmale\tbeijing");
+        System.out.println("hanshunping\rmale\rbeijing");
+    }
+}
